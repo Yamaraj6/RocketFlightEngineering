773194b15679844b04d4b0fe1cb49a2e39e6221f
Aleksander Górka <aleksander.gorka@gmail.com>
2018-08-08 01:19:46 +0200

    camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	[SerializeField] private Transform _target;
+	[SerializeField] private float _smoothSpeed = 0.125f;
+	[SerializeField] private Vector3 _offset;
+
+	private void Awake()
+	{
+		_target = FindObjectOfType<Camera>().transform;
+	}
+
+	private void FixedUpdate()
+	{
+		Follow();
+	}	
+
+	private void Follow()
+	{
+		if (_target == null)
+		{
+			Debug.LogWarning("Target doesn't exist!");
+			return;
+		}
+		
+		var desiredPoint = _target.position + _offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPoint, _smoothSpeed);
+		transform.position = smoothedPosition;
+		transform.LookAt(_target);
+	}
+}
